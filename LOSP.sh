e9a48da10962b0788dba66b3e9d2cfa1b7d99af3
Tillua467 <128046674+tillua467@users.noreply.github.com>
2025-01-02 17:51:53 +0530

    Update LOSP.sh

--- LOSP.sh
+++ LOSP.sh
@@ -10,9 +10,9 @@ echo "========================================================================="
 echo "===================================="
 echo "     Removing Unnecessary Files"
 echo "===================================="
-rm -rf  vendor/xiaomi
-rm -rf  kernel/xiaomi
-rm -rf  device/xiaomi
+rm -rf vendor/xiaomi
+rm -rf kernel/xiaomi
+rm -rf device/xiaomi
 rm -rf device/xiaomi/sm6150-common
 rm -rf vendor/xiaomi/sm6150-common
 rm -rf hardware/xiaomi
@@ -27,28 +27,20 @@ echo "===================================="
 
 # initialize the rom manifest 
 echo "=============================================="
-echo "       Cloning Mniafest..........."
+echo "       Cloning Manifest..........."
 echo "=============================================="
-repo init --depth=1 --no-repo-verify -u https://github.com/yehonatan2020/losp_manifest -b lineage-22.1 --git-lfs
+repo init --depth=1 --no-repo-verify -u https://github.com/Lineage-OS-Special-Project/losp_manifests -b lineage-22.1 --git-lfs
 echo "=============================================="
-echo "       Mniafest Cloned successfully"
+echo "       Manifest Cloned successfully"
 echo "=============================================="
 
 echo "=================================="
 echo "Cloning Necessary trees............"
 echo "=================================="
-# DT
-git clone https://github.com/tillua467/phoenix-dt -b LOS-22 device/xiaomi/phoenix
 # CDT
-git clone https://github.com/tillua467/sm6150-common -b a15 device/xiaomi/sm6150-common
-# KT 
-git clone https://github.com/xiaomi-sm6150/android_kernel_xiaomi_sm6150 -b lineage-22.1 kernel/xiaomi/sm6150
+git clone https://github.com/TheMuppets/proprietary_vendor_sony_kirin vendor/sony
 # VT
-git clone https://github.com/aosp-phoenix/proprietary_vendor_xiaomi_phoenix -b a15 vendor/xiaomi/phoenix
-# CVT 
-git clone https://github.com/tillua467/vendor_sm6150-common -b a15 vendor/xiaomi/sm6150-common
-# HT
-git clone 
-# MIUI CAM
-git clone https://gitlab.com/Shripal17/vendor_xiaomi_miuicamera -b fifteen-leica vendor/xiaomi/miuicamera
-#Viper FX
+git clone https://github.com/TheMuppets/proprietary_vendor_sony_nile-common vendor/sony
+source build/envsetup.sh
+lunch lineage_kirin-ap3a-userdebug
+m bacon
