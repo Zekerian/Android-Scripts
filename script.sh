2b7f65d6ba68a61ae7556eebddd830437cce6496
Tillua467 <128046674+tillua467@users.noreply.github.com>
2025-01-29 03:14:42 +0600

    script: Add new tree to test

--- script.sh
+++ script.sh
@@ -53,11 +53,9 @@ echo "============="
 echo "=============================================="
 echo "       Cloning Trees..........."
 echo "=============================================="
-git clone https://github.com/tillua467/phoenix-dt.git device/xiaomi/phoenix || { echo "Failed to clone device tree"; exit 1; }
-git clone https://github.com/xiaomi-sm6150/android_kernel_xiaomi_sm6150 kernel/xiaomi/sm6150 || { echo "Failed to clone kernel"; exit 1; }
-git clone https://github.com/tillua467/sm6150-common.git device/xiaomi/sm6150-common || { echo "Failed to clone sm6150-common"; exit 1; }
-git clone https://github.com/aosp-phoenix/proprietary_vendor_xiaomi_phoenix.git vendor/xiaomi/phoenix || { echo "Failed to clone vendor phoenix"; exit 1; }
-git clone https://github.com/aosp-phoenix/proprietary_vendor_xiaomi_sm6150-common.git vendor/xiaomi/sm6150-common || { echo "Failed to clone sm6150-common vendor"; exit 1; }
+git clone https://github.com/tillua467/phoenix.git device/xiaomi/phoenix || { echo "Failed to clone device tree"; exit 1; }
+git clone https://github.com/shripal17/android_kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150 || { echo "Failed to clone kernel"; exit 1; }
+git clone https://github.com/shripal17/android_vendor_xiaomi_phoenix.git vendor/xiaomi/phoenix || { echo "Failed to clone vendor phoenix"; exit 1; }
 git clone https://github.com/rom-vayu/android_hardware_xiaomi.git hardware/xiaomi || { echo "Failed to clone hardware"; exit 1; }
 git clone https://gitlab.com/Shripal17/vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera || { echo "Failed to clone MIUI Camera"; exit 1; }
 /opt/crave/resync.sh
