557063a143188bdfdd75b9e5b76847b6ab873f6c
Tillua467 <128046674+tillua467@users.noreply.github.com>
2025-03-02 22:19:01 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,83 +1,9 @@
-#!/bin/bash
-# crave run --no-patch -- "curl https://raw.githubusercontent.com/tillua467/Android-Scripts/refs/heads/main/script.sh | bash"
+. build/envsetup.sh
+# Lunch
+lunch lineage_phoenix-ap3a-userdebug || lunch lineage_phoenix-ap2a-userdebug
+echo =============
 
-# Remove Unnecessary Files
-echo "===================================="
-echo "     Removing Unnecessary Files"
-echo "===================================="
-
-dirs_to_remove=(
-  "vendor/xiaomi"
-  "kernel/xiaomi"
-  "device/xiaomi"
-  "device/xiaomi/sm6150-common"
-  "vendor/xiaomi/sm6150-common"
-  "hardware/xiaomi"
-  "out/target/product/*/*zip"
-  "out/target/product/*/*txt"
-  "out/target/product/*/boot.img"
-  "out/target/product/*/recovery.img"
-  "out/target/product/*/super*img"
-)
-
-for dir in "${dirs_to_remove[@]}"; do
-  [ -e "$dir" ] && rm -rf "$dir"
-done
-
-echo "===================================="
-echo "  Removing Unnecessary Files Done"
-echo "===================================="
-
-# Initialize repo
-echo "=============================================="
-echo "         Cloning Manifest..........."
-echo "=============================================="
-if ! repo init -u https://github.com/AxionAOSP/android.git -b lineage-22.1 --git-lfs; then
-  echo "Repo initialization failed. Exiting."
-  exit 1
-fi
-echo "=============================================="
-echo "       Manifest Cloned successfully"
-echo "=============================================="
-
-# Sync
-if ! /opt/crave/resync.sh || ! repo sync ; then
-  echo "Repo sync failed. Exiting."
-  exit 1
-fi
-echo "============="
-echo " Sync success"
-echo "============="
-
-rm -rf hardware/xiaomi
-
-# Clone device trees and other dependencies
-# Local manifests
-echo "=================================="
-echo "Cloning Local manifest............"
-echo "=================================="
-repo init --no-repo-verify https://github.com/tillua467/local_manifests .repo/local_manifests
-echo "=================================="
-echo "Local manifest cloned successfully"
-echo "=================================="
-/opt/crave/resync.sh
-
-# Export Environment Variables
-echo "======= Exporting........ ======"
-export BUILD_USERNAME=tillua467
-export BUILD_HOSTNAME=crave
-export TARGET_DISABLE_EPPE=true
-export TZ=Asia/Dhaka
-export ALLOW_MISSING_DEPENDENCIES=true
-echo "======= Export Done ======"
-
-# Set up build environment
-echo "====== Starting Envsetup ======="
-source build/envsetup.sh || { echo "Envsetup failed"; exit 1; }
-echo "====== Envsetup Done ======="
-
-# Build ROM
-echo "===================================="
-echo "        Build axion.."
-echo "===================================="
-breakfast phoenix userdebug && brunch phoenix userdebug
+# Make cleaninstall
+axion phoenix userdebug
+axion phoenix gms pico
+brunch phoenix
