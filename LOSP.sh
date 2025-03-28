3b22a3152a5998389382b730ff99ccd542933920
Zeke <zekelooby@gmail.com>
2025-03-28 19:27:34 +0300

    repo init using --depth=1

--- LOSP.sh
+++ LOSP.sh
@@ -34,7 +34,7 @@ echo "===================================="
 echo "=============================================="
 echo "         Cloning Manifest..........."
 echo "=============================================="
-if ! repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault; then
+if ! repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault; then
   echo "Repo initialization failed. Exiting."
   exit 1
 fi
